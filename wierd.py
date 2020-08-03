95778373c50d8d52bf5b2dc0cdb18e8646ef1cc7
Anil Kumar Yadav K <anil.kumar.k.yadav@ericsson.com>
2020-08-03 16:08:01 +0530

    changed printing to one line

--- wierd.py
+++ wierd.py
@@ -1,13 +1,12 @@
 def weird(num):
     while True:
-        print(num)
+        print(num, end=' ')
         if num==1:
             break
         elif num%2==0:
-            num=num/2
+            num=num//2
         else:
             num=3*num+1
 
-if __name__ == "__main__":
-    num=int(input("Enter the number : "))
-    weird(num)
+num=int(input("Enter the number : "))
+weird(num)
